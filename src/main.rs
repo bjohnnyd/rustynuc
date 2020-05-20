18e9415af0cfc9951ee27a99ea6ba86e500c1e88
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2020-05-20 22:02:47 +0100

    playing around with suffixes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::HashMap;
+
+pub struct SuffixTrie<'a>{
+    pub text: String,
+    pub suffix_trie: HashMap<char, &'a str>,
+}
+
+impl SuffixTrie<'_> {
+    fn new(s: &str) -> Self {
+        s.
+
+    }
+
+}
+
+/*
+ append '$' to t
+set a hashmap to store a char --> remainder slice mapping
+for i in length of t
+curent dictionary = the hashmap
+for character in t[i]
+if character not in dictionary
+add an empty vec
+otherwise
+set current_dictionary = current_dictionary[c]
+*/
+
 fn main() {
     println!("Hello, world!");
 }
