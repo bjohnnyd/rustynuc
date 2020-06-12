51a2187e23d4120087cbc9692164400429f8b507
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2020-06-13 00:14:15 +0100

    started on dev of logic

--- oxo/src/lib.rs
+++ oxo/src/lib.rs
@@ -1,7 +1,15 @@
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 #![allow(dead_code, unused_variables)]
 
-//! Crate for intepreting various nucleotide damage related to oxidation (e.g. `oxo-G`)
+//! Crate for intepreting various nucleotide damage related to oxidation (e.g. `oxo-G`).
+//! `oxo-G` damage results generates technical artefacts that cause `G > T` errors first
+//! read and `C -> A` erros in the second read.  In addition, there is an enrichment in
+//! specific sequnce contexts as `CCG -> CAG`.
+//!
+
+mod motif_counts;
+
+const DEFAULT_KMER_SIZE: usize = 21;
 
 #[cfg(test)]
 mod tests {
