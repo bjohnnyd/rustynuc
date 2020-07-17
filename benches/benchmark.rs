4df3d252a91953f455e69fc3c836faa4a4484963
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2020-07-17 19:58:57 +0100

    added some benches

---
+++ benches/benchmark.rs
@@ -0,0 +1,22 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use log::error;
+
+fn is_s(seq: &[u8], pos: usize) -> bool {
+    if pos >= seq.len() {
+        error!("Reference sequence is shorter than BAM alignment positions");
+        std::process::exit(1)
+    } else {
+        match seq[pos] {
+            b'G' | b'C' | b'g' | b'c' => true,
+            _ => false,
+        }
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let seq = b"acGTCttACGaTA";
+    c.bench_function("is_s G", |b| b.iter(|| is_s(seq, 3)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
